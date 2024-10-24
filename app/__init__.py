d00dd5dbaa99397521bb303b58e8f73af21d2840
Henrique Monteiro <80502511+BarryHook1@users.noreply.github.com>
2024-10-24 20:15:35 -0300

    Finalizado

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask
 from .main import main_bp
 from app.auth.routes import auth_bp
 from pymongo import MongoClient
+from datetime import timedelta
 
 def create_app():
     app = Flask(__name__)
+    
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=90)
+    app.config['SESSION_PERMANENT'] = True
 
     # Configurações da aplicação
     app.config.from_object('config.Config')
--- app/main/routes.py
+++ app/main/routes.py
@@ -7,9 +7,6 @@ from bson import ObjectId
 import smtplib
 from email.mime.text import MIMEText
 
-@main_bp.before_request
-def clear_session_on_restart():
-    session.clear()  # Limpa a sessão ao reiniciar
 
 @main_bp.route('/')
 def index():
